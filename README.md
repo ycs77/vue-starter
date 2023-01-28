8079c6b4ff09cfe5037f687f38733447472ae868
Lucas Yang <yangchenshin77@gmail.com>
2023-01-28 21:50:49 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Lucas Yang's Vue Starter
+
+> Apply this starter: `npx degit ycs77/vue-starter`
+
+## Installation
+
+```bash
+yarn
+yarn dev
+```
---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { HeadlessUiResolver } from 'unplugin-vue-components/resolvers'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Pages from 'vite-plugin-pages'
+import { HeadlessUiFloatResolver } from '@headlessui-float/vue'
+
+export default defineConfig({
+  plugins: [
+    Vue(),
+    Components({
+      resolvers: [
+        HeadlessUiResolver(),
+        HeadlessUiFloatResolver(),
+        IconsResolver({ prefix: '' }),
+      ],
+    }),
+    Pages(),
+    Icons(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
