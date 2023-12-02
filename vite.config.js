3822c8f72aa0706984409603d8b75b80633fbd89
Lucas Yang <yangchenshin77@gmail.com>
2023-12-02 23:56:31 +0800

    Remove headlessui float

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import { HeadlessUiResolver } from 'unplugin-vue-components/resolvers'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import Pages from 'vite-plugin-pages'
-import { HeadlessUiFloatResolver } from '@headlessui-float/vue'
 
 export default defineConfig({
   plugins: [
@@ -18,7 +17,6 @@ export default defineConfig({
     Components({
       resolvers: [
         HeadlessUiResolver(),
-        HeadlessUiFloatResolver(),
         IconsResolver({ prefix: '' }),
       ],
     }),
