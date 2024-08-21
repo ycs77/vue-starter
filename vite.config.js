2a1bd1e51d3fd8ebf39d7a9a56fb301d81a08c00
Lucas Yang <yangchenshin77@gmail.com>
2024-08-21 22:02:21 +0800

    Replace Headless UI with Radix Vue

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,18 @@ import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
-import { HeadlessUiResolver } from 'unplugin-vue-components/resolvers'
 import Pages from 'vite-plugin-pages'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
+import RadixVueResolver from 'radix-vue/resolver'
 
 export default defineConfig({
   plugins: [
     Vue(),
     Components({
       resolvers: [
-        HeadlessUiResolver(),
         IconsResolver({ prefix: '' }),
+        RadixVueResolver(),
       ],
     }),
     Pages(),
