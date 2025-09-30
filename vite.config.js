22364a409fb666b3eca3e0df0a3b7a3dd008da93
Lucas Yang <yangchenshin77@gmail.com>
2025-10-01 03:30:10 +0800

    Bump dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,20 @@ import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
-import TailwindCss from '@tailwindcss/vite'
+import TailwindCSS from '@tailwindcss/vite'
 import Components from 'unplugin-vue-components/vite'
 import Pages from 'vite-plugin-pages'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
-import RadixVueResolver from 'radix-vue/resolver'
 
 export default defineConfig({
   plugins: [
     Vue(),
     VueDevTools(),
-    TailwindCss(),
+    TailwindCSS(),
     Components({
       resolvers: [
         IconsResolver({ prefix: '' }),
-        RadixVueResolver(),
       ],
     }),
     Pages(),
