cc8afd0521dd7d8220638e2f0ff25a754c4eb08b
Lucas Yang <yangchenshin77@gmail.com>
2026-01-05 17:10:03 +0800

    Migrate to unplugin-vue-router

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import TailwindCSS from '@tailwindcss/vite'
 import Components from 'unplugin-vue-components/vite'
-import Pages from 'vite-plugin-pages'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 
 export default defineConfig({
   plugins: [
+    VueRouter(),
     Vue(),
     VueDevTools(),
     TailwindCSS(),
@@ -18,7 +19,6 @@ export default defineConfig({
         IconsResolver({ prefix: '' }),
       ],
     }),
-    Pages(),
     Icons(),
   ],
   resolve: {
