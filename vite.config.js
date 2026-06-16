29de5393a65b4293bb23b9c7c2d5621cbadb84e2
Lucas Yang <yangchenshin77@gmail.com>
2026-06-16 13:48:39 +0800

    Uninstall vue devtools

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import VueRouter from 'vue-router/vite'
-import VueDevTools from 'vite-plugin-vue-devtools'
 import TailwindCSS from '@tailwindcss/vite'
 import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
@@ -12,7 +11,6 @@ export default defineConfig({
   plugins: [
     VueRouter(),
     Vue(),
-    VueDevTools(),
     TailwindCSS(),
     Components({
       resolvers: [
